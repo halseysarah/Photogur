fcc744a94b53e5f1f209a8b0ad1e21788db267ab
SARAH <HALSEYSARAH@Gmail.com>
2014-08-26 15:43:40 -0400

    Refactoring step #4 to use dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+  get 'pictures/:id' =>'pictures#shows', as: "picture"
+end
 
+=begin
   get 'pictures/0' => 'pictures#picture0'
   get 'pictures/1' => 'pictures#picture1'
   get 'pictures/2' => 'pictures#picture2'
-  
+=end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -59,4 +63,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
